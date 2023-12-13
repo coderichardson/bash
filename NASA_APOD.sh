c66a4667b312301cb387e9fff973294e1c486917
Cody Richardson <63025749+coderichardson@users.noreply.github.com>
2023-12-12 22:23:03 -0500

    Ensuring correct filename extension
    Checks the extension on the image so when the file is saved the correct extension is made part of the filename.

--- NASA_APOD.sh
+++ NASA_APOD.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Download NASA's Astronomy Picture of the Day!
+
 # Get today's date in the format YYYYMMDD
 today=$(date +%Y%m%d)
 
@@ -15,11 +17,14 @@ html_content=$response
 # Extract the string following 'IMG SRC=' using grep and regex
 img_src_string=$(echo "$html_content" | grep -oP '<IMG\s+SRC="\K[^"]+' | head -1)
 
-# Download the image and name is today's date and assign a filetype of .jpg.
-curl -s -o "$today.jpg" "https://apod.nasa.gov/apod/$img_src_string"
+# Extract the image extension from the URL
+img_extension=$(echo "$img_src_string" | grep -oP '\.\w+$' | tr '[:upper:]' '[:lower:]')
+
+# Download the image with the identified extension
+curl -s -o "$today$img_extension" "https://apod.nasa.gov/apod/$img_src_string"
 
 # Print that the download was successful.
-file_name=$today.jpg
+file_name=$today$img_extension
 if [ -f "$file_name" ]; then
     echo "File '$file_name' downloaded successfully."
 else
